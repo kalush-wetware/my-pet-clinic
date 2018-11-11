ab08bbf5a9af95e9af701d5594969e75c503cfc3
ada2 <ada@anywhere.net>
2018-11-11 17:33:31 +0300

    Owner Controller is created. close #13

--- my-pet-clinic-web/src/main/java/net/ada/mypetclinic/controllers/OwnerController.java
+++ my-pet-clinic-web/src/main/java/net/ada/mypetclinic/controllers/OwnerController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
-
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners() {
         return "owners/index";
