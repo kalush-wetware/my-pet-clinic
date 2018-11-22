50f94d96423ab3b7605716843bc6ea836170f609
ada2 <ada@anywhere.net>
2018-11-22 23:04:55 +0300

    Add Contact Info properties to Owner. Close #28

--- my-pet-clinic-data/src/main/java/net/ada/mypetclinic/model/Owner.java
+++ my-pet-clinic-data/src/main/java/net/ada/mypetclinic/model/Owner.java
@@ -3,7 +3,34 @@ package net.ada.mypetclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String teleophone;    
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTeleophone() {
+        return teleophone;
+    }
+
+    public void setTeleophone(String teleophone) {
+        this.teleophone = teleophone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
@@ -12,6 +39,7 @@ public class Owner extends Person {
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
+
     
     
 }
