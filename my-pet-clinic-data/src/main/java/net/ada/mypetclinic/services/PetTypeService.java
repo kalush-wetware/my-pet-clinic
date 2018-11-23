4b4d4258f13699371d42280b12a0d95cc4889d1c
Ada <ada@anywhere.net>
2018-11-23 12:39:36 +0300

    Create Pet Type Map Service Impl. Close #29

---
+++ my-pet-clinic-data/src/main/java/net/ada/mypetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package net.ada.mypetclinic.services;
+
+import net.ada.mypetclinic.model.PetType;
+
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+    
+}
---
+++ my-pet-clinic-data/src/main/java/net/ada/mypetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package net.ada.mypetclinic.services.map;
+
+import java.util.Set;
+import net.ada.mypetclinic.model.PetType;
+import net.ada.mypetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService{
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
