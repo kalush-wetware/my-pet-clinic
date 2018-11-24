6050d050b23de65bbedbcbd6e97976868e766b2c
ada2 <ada@anywhere.net>
2018-11-24 21:23:00 +0300

    Create specialities on Bootstrap.

--- my-pet-clinic-web/src/main/java/net/ada/mypetclinic/bootstrap/DataLoader.java
+++ my-pet-clinic-web/src/main/java/net/ada/mypetclinic/bootstrap/DataLoader.java
@@ -4,14 +4,15 @@ import java.time.LocalDate;
 import net.ada.mypetclinic.model.Owner;
 import net.ada.mypetclinic.model.Pet;
 import net.ada.mypetclinic.model.PetType;
+import net.ada.mypetclinic.model.Speciality;
 import net.ada.mypetclinic.model.Vet;
 import net.ada.mypetclinic.services.OwnerService;
+import net.ada.mypetclinic.services.PetService;
 import net.ada.mypetclinic.services.PetTypeService;
 import net.ada.mypetclinic.services.VetService;
-import net.ada.mypetclinic.services.map.OwnerServiceMap;
-import net.ada.mypetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import net.ada.mypetclinic.services.SpecialityService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -19,30 +20,53 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
-     
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    private final SpecialityService specialityService;
+
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialityService = specialityService;
     }
 
     @Override
-    public void run(String... args) throws Exception {    
+    public void run(String... args) throws Exception {
+        int count = petTypeService.findAll().size();
+
+        if (count == 0) {
+            loadData();
+        }
+
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
-        
+
         PetType cat = new PetType();
         cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
-                
+
+        Speciality radiology = new Speciality();
+        radiology.setDescription("Radiology");
+        Speciality savedRadiology = specialityService.save(radiology);
+
+        Speciality surgery = new Speciality();
+        surgery.setDescription("Surgery");
+        Speciality savedSurgery = specialityService.save(surgery);
+
+        Speciality dentistry = new Speciality();
+        dentistry.setDescription("Dentistry");
+        Speciality savedDentistry = specialityService.save(dentistry);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
         owner1.setAddress("London Street");
         owner1.setCity("London");
         owner1.setTeleophone("112-2234");
-        
+
         Pet mikesPet = new Pet();
         mikesPet.setPetType(savedDogPetType);
         mikesPet.setOwner(owner1);
@@ -65,25 +89,26 @@ public class DataLoader implements CommandLineRunner {
         fionasPet.setBirthDate(LocalDate.now());
         fionasPet.setName("Slyvester");
         owner2.getPets().add(fionasPet);
-        
+
         ownerService.save(owner2);
-        
+
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
+        vet1.getSpecialities().add(radiology);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
+        vet2.getSpecialities().add(surgery);
 
         vetService.save(vet2);
 
         System.out.println("Loaded Vets....");
-
     }
 
 }
