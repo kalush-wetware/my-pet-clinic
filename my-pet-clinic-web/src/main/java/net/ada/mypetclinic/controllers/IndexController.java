f3bc874f5e81b328500bcc7dad2e487c15eed2fc
ada2 <ada@anywhere.net>
2018-11-24 21:55:17 +0300

    Menu links are broken. Close #45

--- my-pet-clinic-web/src/main/java/net/ada/mypetclinic/controllers/IndexController.java
+++ my-pet-clinic-web/src/main/java/net/ada/mypetclinic/controllers/IndexController.java
@@ -1,14 +1,19 @@
 package net.ada.mypetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
 public class IndexController {
 
-    @RequestMapping({"","/", "index", "index.html"})
+    @RequestMapping({"", "/", "index", "index.html"})
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/oups")
+    public String oupsHandler(Model model) {
+        return "notimplemented";
     }
 }
