d1c5e4875a27523edbe37919b28f6d711951cc6b
ada2 <ada@anywhere.net>
2018-11-29 11:39:15 +0300

    Convert Owners to JPA entities. Close #36

--- my-pet-clinic-data/src/main/java/net/ada/mypetclinic/model/Owner.java
+++ my-pet-clinic-data/src/main/java/net/ada/mypetclinic/model/Owner.java
@@ -2,11 +2,26 @@ package net.ada.mypetclinic.model;
 
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="owners")
 public class Owner extends Person {
+    
+    @Column(name="address")
     private String address;
+    
+    @Column(name="city")
     private String city;
-    private String teleophone;    
+    
+    @Column(name="telephone")
+    private String telephone;    
+    
+    @OneToMany(cascade=CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
@@ -25,20 +40,21 @@ public class Owner extends Person {
         this.city = city;
     }
 
-    public String getTeleophone() {
-        return teleophone;
-    }
-
-    public void setTeleophone(String teleophone) {
-        this.teleophone = teleophone;
-    }
-
+   
     public Set<Pet> getPets() {
         return pets;
     }
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     
