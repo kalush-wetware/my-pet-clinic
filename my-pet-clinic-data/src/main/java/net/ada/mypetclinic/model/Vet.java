7bdf7b195f17d3a06989e63ae0911c745e3e6ac4
ada2 <ada@anywhere.net>
2018-12-04 22:31:28 +0300

    Add Project Lombok and Refactor. Close #51

--- my-pet-clinic-data/src/main/java/net/ada/mypetclinic/model/Vet.java
+++ my-pet-clinic-data/src/main/java/net/ada/mypetclinic/model/Vet.java
@@ -8,7 +8,17 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name="vets")
 public class Vet  extends Person{
@@ -16,13 +26,5 @@ public class Vet  extends Person{
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
-    
     
 }
