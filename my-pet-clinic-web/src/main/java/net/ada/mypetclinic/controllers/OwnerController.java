53cc64442d7402ec5d407df25855f9469720c92d
ada2 <ada@anywhere.net>
2018-12-22 21:39:11 +0300

    Error On Display Owner. Close #61

--- my-pet-clinic-web/src/main/java/net/ada/mypetclinic/controllers/OwnerController.java
+++ my-pet-clinic-web/src/main/java/net/ada/mypetclinic/controllers/OwnerController.java
@@ -3,27 +3,32 @@ package net.ada.mypetclinic.controllers;
 import net.ada.mypetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
-    
+
     private final OwnerService ownerService;
 
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
     }
-    
+
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
-    
-    @RequestMapping("/find")
-    public String findOwners(Model model) {
-        return "notimplemented";
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
-    
+
 }
